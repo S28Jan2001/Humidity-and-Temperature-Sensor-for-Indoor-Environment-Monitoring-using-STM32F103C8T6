825f941a2e8fe812b29264cf487eaf45ef4fac70
S28Jan2001 <133202721+S28Jan2001@users.noreply.github.com>
2024-12-02 14:02:22 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,120 @@
+/* USER CODE BEGIN Includes */
+#include "fonts.h"
+#include "ssd1306.h"
+#include "stdio.h"
+/* USER CODE END Includes */
+
+/* USER CODE BEGIN 0 */
+#define DHT11_PORT GPIOB
+#define DHT11_PIN GPIO_PIN_9
+uint8_t RHI, RHD, TCI, TCD, SUM;
+uint32_t pMillis, cMillis;
+float tCelsius = 0;
+float tFahrenheit = 0;
+float RH = 0;
+uint8_t TFI = 0;
+uint8_t TFD = 0;
+char strCopy[15];
+
+void microDelay (uint16_t delay)
+{
+  __HAL_TIM_SET_COUNTER(&htim1, 0);
+  while (__HAL_TIM_GET_COUNTER(&htim1) < delay);
+}
+
+uint8_t DHT11_Start (void)
+{
+  uint8_t Response = 0;
+  GPIO_InitTypeDef GPIO_InitStructPrivate = {0};
+  GPIO_InitStructPrivate.Pin = DHT11_PIN;
+  GPIO_InitStructPrivate.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStructPrivate.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStructPrivate.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(DHT11_PORT, &GPIO_InitStructPrivate); // set the pin as output
+  HAL_GPIO_WritePin (DHT11_PORT, DHT11_PIN, 0);   // pull the pin low
+  HAL_Delay(20);   // wait for 20ms
+  HAL_GPIO_WritePin (DHT11_PORT, DHT11_PIN, 1);   // pull the pin high
+  microDelay (30);   // wait for 30us
+  GPIO_InitStructPrivate.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStructPrivate.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(DHT11_PORT, &GPIO_InitStructPrivate); // set the pin as input
+  microDelay (40);
+  if (!(HAL_GPIO_ReadPin (DHT11_PORT, DHT11_PIN)))
+  {
+    microDelay (80);
+    if ((HAL_GPIO_ReadPin (DHT11_PORT, DHT11_PIN))) Response = 1;
+  }
+  pMillis = HAL_GetTick();
+  cMillis = HAL_GetTick();
+  while ((HAL_GPIO_ReadPin (DHT11_PORT, DHT11_PIN)) && pMillis + 2 > cMillis)
+  {
+    cMillis = HAL_GetTick();
+  }
+  return Response;
+}
+
+uint8_t DHT11_Read (void)
+{
+  uint8_t a,b;
+  for (a=0;a<8;a++)
+  {
+    pMillis = HAL_GetTick();
+    cMillis = HAL_GetTick();
+    while (!(HAL_GPIO_ReadPin (DHT11_PORT, DHT11_PIN)) && pMillis + 2 > cMillis)
+    {  // wait for the pin to go high
+      cMillis = HAL_GetTick();
+    }
+    microDelay (40);   // wait for 40 us
+    if (!(HAL_GPIO_ReadPin (DHT11_PORT, DHT11_PIN)))   // if the pin is low
+      b&= ~(1<<(7-a));
+    else
+      b|= (1<<(7-a));
+    pMillis = HAL_GetTick();
+    cMillis = HAL_GetTick();
+    while ((HAL_GPIO_ReadPin (DHT11_PORT, DHT11_PIN)) && pMillis + 2 > cMillis)
+    {  // wait for the pin to go low
+      cMillis = HAL_GetTick();
+    }
+  }
+  return b;
+}
+/* USER CODE END 0 */
+
+  /* USER CODE BEGIN 2 */
+  HAL_TIM_Base_Start(&htim1);
+  SSD1306_Init();
+  /* USER CODE END 2 */
+
+ /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    if(DHT11_Start())
+    {
+      RHI = DHT11_Read(); // Relative humidity integral
+      RHD = DHT11_Read(); // Relative humidity decimal
+      TCI = DHT11_Read(); // Celsius integral
+      TCD = DHT11_Read(); // Celsius decimal
+      SUM = DHT11_Read(); // Check sum
+      if (RHI + RHD + TCI + TCD == SUM)
+      {
+        // Can use RHI and TCI for any purposes if whole number only needed
+        tCelsius = (float)TCI + (float)(TCD/10.0);
+        tFahrenheit = tCelsius * 9/5 + 32;
+        RH = (float)RHI + (float)(RHD/10.0);
+        // Can use tCelsius, tFahrenheit and RH for any purposes
+        TFI = tFahrenheit;  // Fahrenheit integral
+        TFD = tFahrenheit*10-TFI*10; // Fahrenheit decimal
+        sprintf(strCopy,"%d.%d C   ", TCI, TCD);
+        SSD1306_GotoXY (0, 0);
+        SSD1306_Puts (strCopy, &Font_11x18, 1);
+        sprintf(strCopy,"%d.%d F   ", TFI, TFD);
+        SSD1306_GotoXY (0, 20);
+        SSD1306_Puts (strCopy, &Font_11x18, 1);
+        sprintf(strCopy,"%d.%d %%  ", RHI, RHD);
+        SSD1306_GotoXY (0, 40);
+        SSD1306_Puts (strCopy, &Font_11x18, 1);
+        SSD1306_UpdateScreen();
+      }
+    }
+    HAL_Delay(2000);
+    /* USER CODE END WHILE */
